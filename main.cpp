369ffb27a135c33d45a0b0e3e1947c4af3fb4a68
Huynh Phu <phuhuynhquoc99@gmail.com>
2020-03-26 23:14:17 +0700

    complete section 4 - understand every thing

--- main.cpp
+++ main.cpp
@@ -1,22 +1,43 @@
+#include "lib\ray.h"
+
+
 #include <iostream>
+#include <fstream>
+
+
+using namespace std;
+
+vec3 ray_color(const ray &r){
+	vec3 unit_direction = unit_vector(r.direction());
+	auto t = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0 - t) * vec3(1.0,1.0,1.0) + t * vec3(0.5,0.7,1.0);
+}
 
 int main() {
     const int image_width = 200;
     const int image_height = 100;
+	
+	
+	ofstream outfile("render.ppm",ios_base::out);
 
+	outfile << "P3\n" << image_width << " " << image_height << "\n255\n";
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+	
+	vec3 lower_left_corner(-2.0, -1.0, -1.0);
+    vec3 horizontal(4.0, 0.0, 0.0);
+    vec3 vertical(0.0, 2.0, 0.0);
+    vec3 origin(0.0, 0.0, 0.0);
 
     for (int j = image_height-1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / image_width;
-            auto g = double(j) / image_height;
-            auto b = 0.2;
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            auto u = double(i) / image_width;
+			auto v = double(j) / image_height;
+			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+			vec3 color = ray_color(r);
+            color.write_color(outfile);
         }
     }
+	
     std::cerr << "\nDone.\n";
 }
